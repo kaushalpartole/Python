3fd2e57d939b61fc08d7b116e7f5efb7c9b04055
kaushalpartole <140429640+kaushalpartole@users.noreply.github.com>
2026-01-30 15:25:21 +0000

    completed the bill splitter mini project

--- Bill_Splitter.py
+++ Bill_Splitter.py
@@ -1,1 +1,6 @@
+Bill = float(input("What is the total bill? "))
+Tip = int(input("How much tip would you like to pay? "))
+People = int(input("How many people to split the bill? "))
 
+Each_People_pays = (Bill + (Bill * (Tip/100)))/People
+print(f"Each People Pays: {round(Each_People_pays,2)}")
